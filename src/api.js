3effc4c0ea0550e517b3010558aa33e3f9886587
James (Robbie) Gathje <robbiegathje@outlook.com>
2023-08-10 21:45:03 -0400

    chore: write API helper class to communicate with backend API from frontend

---
+++ src/api.js
@@ -0,0 +1,24 @@
+class MovieDatabaseAPI {
+	static token;
+	static async register() {}
+	static async login() {}
+	static async changePassword() {}
+	static async changeUsername() {}
+	static async getFavoriteMovies() {}
+	static async addFavoriteMovie() {}
+	static async removeFavoriteMovie() {}
+	static async getFavoriteTv() {}
+	static async addFavoriteTv() {}
+	static async removeFavoriteTv() {}
+	static async getMovie() {}
+	static async updateMovie() {}
+	static async searchMovies() {}
+	static async getTvSeries() {}
+	static async updateTvSeries() {}
+	static async searchTv() {}
+}
+
+MovieDatabaseAPI.token =
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJyb2JiaWUiLCJpYXQiOjE2OTE3MTcwOTl9.rlrND6QeZuZ3Xkp72TxeMOL4k4bXtBOB2QpwyJEHlJc';
+
+export default MovieDatabaseAPI;
